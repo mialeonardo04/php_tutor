80b207f2ac0c5de4b6f095476bf33d95fcf8457e
Ignasius Leonardo <ignasiusleonardo@student.uns.ac.id>
2020-03-02 17:19:15 +0700

    pengajar-courses (10)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,5 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(CourseTableSeeder::class);
-        $this->call(PretestQuestionsTableSeeder::class);
     }
 }
