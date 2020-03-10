7b9feaee30b12b91e7686dfd3bbed22290740d6c
Ignasius Leonardo <ignasiusleonardo@student.uns.ac.id>
2020-03-11 02:43:10 +0700

    footer credit

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,3 +1,4 @@
 	<div class="footer-wrap bg-white pd-20 mb-20 border-radius-5 box-shadow">
-		PHP Intelligent Tutor | Ignasius Agus Leonardo - PTIK Universitas Sebelas Maret 2019
+        PHP Intelligent Tutor | by <a target="_blank" href="https://github.com/leonard04">Ignasius Agus Leonardo</a> - PTIK Universitas Sebelas Maret 2019<br>
+        Bootstrap 4 Admin Template By <a target="_blank" href="https://github.com/dropways">Ankit Hingarajiya</a>
 	</div>
